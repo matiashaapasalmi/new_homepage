8ddecfe66da24ea086ae4d21b334266dd7ae4ed6
Matias <matias.haapasalmi@live.com>
2023-01-15 19:16:44 +0200

    added feature that you can download my cv

--- site/src/pages/CV.js
+++ site/src/pages/CV.js
@@ -1,3 +1,28 @@
 export default function CV() {
-    return <h1>CV</h1>
-}
+    // Function will execute on click of button
+    const onButtonClick = () => {
+        // using Java Script method to get PDF file
+        fetch('cv.pdf').then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Matias Haapasalmi.pdf';
+                alink.click();
+            })
+        })
+    }
+    return (
+        <>
+            <center>
+                <h1>CV</h1>
+                <h3>Click on below button to download my CV as a PDF file</h3>
+                <button onClick={onButtonClick}>
+                    Download CV
+                </button>
+            </center>
+        </>
+    );
+};
